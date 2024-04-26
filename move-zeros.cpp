ddea2df21d5995dc495488cc10e0243e304f0be3
aman-codesss <amanpartelyt@gmail.com>
2024-04-26 15:11:24 +0545

    k =0 i=0

--- move-zeros.cpp
+++ move-zeros.cpp
@@ -1,11 +1,8 @@
 #include<iostream>
 using namespace std;
 void moveZero(int arr[],int n){
-    int k=1;
-    if(arr[0]==0){
-        k=0;
-    }
-    for(int i=1;i<n;i++){
+    int k=0;
+    for(int i=0;i<n;i++){
         if(arr[i]!=0){
             swap(arr[i],arr[k]);
             k++;
@@ -20,8 +17,8 @@ void print(int arr[],int size){
     cout<<endl;
 }
 int main(){
-    // int arr[8]={0,2,1,0,0,0,3,0};
-    int arr[8]={0,0,1,0,9,0,3,0};
+    int arr[8]={2,0,1,0,0,0,3,0};
+    // int arr[8]={0,0,1,0,9,0,3,0};
     moveZero(arr,8);
     print(arr,8);
 
